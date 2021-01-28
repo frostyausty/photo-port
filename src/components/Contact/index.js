5efaa6a4686559feaf302691aa89d76b1f555cf6
frostyausty <priceaustin62@gmail.com>
2021-01-28 15:59:28 -0600

    added testing file for contact component, updated nav test

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -3,8 +3,16 @@ import { validateEmail } from '../../utils/helpers';
 
 function ContactForm() {
     const [formState, setFormState] = useState({ name: '', email: '', message: '' });
-    const { name, email, message }  = formState;
+
     const [errorMessage, setErrorMessage] = useState('');
+    const { name, email, message } = formState;
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        if (!errorMessage) {
+            setFormState({ ...formState, [e.target.name]: e.target.value })
+        }
+    };
 
     function handleChange(e) {
         if (e.target.name === 'email') {
@@ -14,26 +22,20 @@ function ContactForm() {
             if (!isValid) {
                 setErrorMessage('Your email is invalid.');
             } else {
-                if (!e.target.value.length) {
-                    setErrorMessage(`${e.target.name} is required.`);
-                } else {
-                    setErrorMessage('');
-                }
+                setErrorMessage('');
+            }
+        } else {
+            if (!e.target.value.length) {
+                setErrorMessage(`${e.target.name} is required.`);
+            } else {
+                setErrorMessage('');
             }
         }
-        if (!errorMessage) {
-            setFormState({ ...formState, [e.target.name]: e.target.value })
-        }
-    }
+    };
 
-    function handleSubmit(e) {
-        e.preventDefault();
-        console.log(formState);
-    }
-
-    return(
+    return (
         <section>
-            <h1>Contact me</h1>
+            <h1 data-testid="h1tag">Contact me</h1>
             <form id="contact-form" onSubmit={handleSubmit}>
                 <div>
                     <label htmlFor="name">Name:</label>
@@ -52,7 +54,7 @@ function ContactForm() {
                         <p className="error-text">{errorMessage}</p>
                     </div>
                 )}
-                <button type="submit">Submit</button>
+                <button data-testid="button" type="submit">Submit</button>
             </form>
         </section>
     );
